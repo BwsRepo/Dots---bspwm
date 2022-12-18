08361f566ea0e717f9750b115f6d006321fbc371
BwsRepo <89018551+BwsRepo@users.noreply.github.com>
2022-12-18 10:45:37 +0100

    Add files via upload

---
+++ Dots/eww/misc/eww.s
@@ -0,0 +1,253 @@
+*{
+    //all: unset;
+    font-family: "Comic Mono";
+}
+
+scale trough {
+    background-color:#857e7e; 
+    border-radius: 50px;
+    min-height: 10px;
+    min-width: 100px;
+    margin: 0 20px 0 20px;
+  }
+  scale trough highlight {
+    all: unset;
+    background-color: #ffffff; 
+    border-radius: 10px;
+  }
+
+scrollbar {
+    background-color:#857e7e; 
+    border-radius: 50px;
+    min-height: 10px;
+    min-width: 100px;
+    margin: 0 20px 0 20px;
+}
+
+scrollbar slider{
+    background-color: #ffffff; 
+    border-radius: 50px;
+    min-height: 10px;
+    min-width: 10px;
+    margin: 0 20px 0 20px;
+}
+
+
+.bar, .scroll_box{
+    background-color: #161616;
+    border-radius: 16px;
+    border-style: solid;
+    border-width: 2px;
+}
+
+.music_pop{
+    background-color: #161616;
+    border-style: solid;
+    border-radius: 16px;
+    border-width: 2px;
+}
+
+.powermenu{
+    background-color: #161616;
+    border-style: solid;
+    border-radius: 16px;
+    border-width: 2px;    
+}
+
+.volmenu{
+    background-color: #161616;
+    border-style: solid;
+    border-radius: 16px;
+    border-width: 2px;    
+}
+.cal{
+    background-color: #161616;
+    border-style: solid;
+    border-radius: 16px;
+    border-width: 2px;      
+}
+.workspaces {
+    margin-top: 15px;
+    margin-left: 20px;
+    font-size: 17px;
+}
+
+.music_btn_prev{
+    font-family: "Iosevka Nerd Font";
+    font-size: 22px;
+    margin-right: 10px;
+}
+
+.music_img_btn{
+    background-size: 25px;	
+	min-height: 25px;
+	min-width: 25px;
+	border-radius: 5px ;}
+
+.music_img_title{
+    background-size: 30px;	
+    min-height: 30px;
+    min-width: 30px;
+    border-radius: 5px ;}
+
+.music_title_label{
+    margin-left: 10px;
+    font-size: 15px;
+}
+
+.music_btn_next{
+    font-family: "Iosevka Nerd Font";
+    font-size: 22px;
+    margin-left: 10px;
+}
+
+.time_outer{
+    margin-right: 7px;
+}
+
+.time_btn{
+    margin-right: 15px;
+}
+
+.time_label_hour{
+    font-size: 18px;
+    margin-top: 5px;
+    margin-bottom: -5px;
+}
+
+.time_label_min{
+    font-size: 16px;
+    margin-top: -8px;
+}
+
+
+.power_btn{
+    font-family: "Iosevka Nerd Font";
+    font-size: 30px;
+    color: #e56d76;
+}
+
+.power_btn:hover{
+    color: #ed999f;
+    margin-bottom: 5px;
+}
+
+.powermenu_btn{
+    font-family: "Iosevka Nerd Font";
+    font-size: 55px;
+    color: #857e7e;
+    margin-bottom: 10px;
+    margin-top: -20px;
+}  
+
+.powermenu_btn:hover{
+    font-family: "Iosevka Nerd Font";
+    color: #ffffff;
+    margin-bottom: -5px;
+}
+
+
+.power_m_label{
+    font-size: 20px;
+    margin-top: 10px;
+    margin-bottom: 30px;
+}
+
+.musp_art{
+    border-radius: 16px;
+    margin-right: 30px;
+    margin-top: 30px;
+    margin-left: 30px;
+    margin-bottom: 15px;
+    background-size: 200px;
+    min-width: 200px;
+    min-height: 200px;
+}
+
+.musp_title{
+    font-size: 20px;
+    color: #ffffff;}
+
+.musp_artist{
+    font-size: 15px;
+    color: #857e7e;
+    margin-top: 10px;
+}
+
+.slider_bx{
+    margin-top: 15px;
+}
+
+.musp_btn{
+    margin-top: 10px;
+    font-size: 30px;
+}
+
+.mycal{
+    margin: 10px;
+}
+
+.graph{
+    margin: 5px;
+}
+
+.ram{
+    color: #969b62;
+    background-color: #857e7e;
+    border-radius: 10px;
+    border-color: #ffffff; 
+}
+
+.cpu{
+    color: #76d7d6;
+    background-color: #857e7e;
+    border-radius: 10px;
+    border-color: #ffffff;   
+}
+
+.lab_box{
+    margin-left: -70px;
+}
+
+.vol, .mic{
+    font-size: 40px;
+}
+
+.vol_sliderbx{
+    margin-left: -80px;
+    margin-right:10px
+}
+
+.vol_box_btn{
+    margin-right: -15px;
+    margin-left: -10px;
+}
+
+.vol_box_btn:hover{
+    margin-bottom: 10px;
+    margin-top: -20px;
+}
+
+.vol_btn{
+    font-size: 30px;
+}
+
+.game_icon{
+    border-radius: 16px;
+    margin-right: 20px;
+    margin-top: 0px;
+    margin-left: 20px;
+    margin-bottom: 0px;
+    background-size: 200px;
+    min-width: 200px;
+    min-height: 288px;
+}
+
+
+.scroll_outer{
+    margin: 10px;
+}
+
+.scroll{
+    background-color: #857e7e;
+}
